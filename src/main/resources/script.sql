421ef1a9999b50c7e7f06a2c82c1a49499c4cb0f
Sudeep Krishna <sudeepkrishna@Sudeeps-MacBook-Pro.local>
2020-10-04 16:14:00 +0530

    Transaction 7 done

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -11,3 +11,5 @@ import into orders(O_W_ID , O_D_ID , O_ID , O_C_ID , O_CARRIER_ID , O_OL_CNT , O
 import into orderline(OL_W_ID, OL_D_ID , OL_O_ID , OL_NUMBER , OL_I_ID , OL_DELIVERY_D , OL_AMOUNT , OL_SUPPLY_W_ID , OL_QUANTITY , OL_DIST_INFO ) CSV DATA('nodelocal://1/order-line.csv');
 
 import into stock(S_W_ID , S_I_ID , S_QUANTITY , S_YTD , S_ORDER_CNT , S_REMOTE_CNT , S_DIST_01 , S_DIST_02 , S_DIST_03 , S_DIST_04 , S_DIST_05, S_DIST_06 , S_DIST_07 , S_DIST_08 , S_DIST_09 , S_DIST_10 , S_DATA ) CSV DATA('nodelocal://1/stock.csv');
+
+create index on customer(c_balance desc);
